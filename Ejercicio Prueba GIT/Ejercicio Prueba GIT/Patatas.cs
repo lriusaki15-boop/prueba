000aae9f762db2116c71cba5e96ae5e5b1e7a270
Miguel Cervera <l.riusaki15@gmail.com>
2026-03-30 12:43:55 +0200

    fet:nuevos cambios

--- Ejercicio Prueba GIT/Ejercicio Prueba GIT/Patatas.cs
+++ Ejercicio Prueba GIT/Ejercicio Prueba GIT/Patatas.cs
@@ -7,5 +7,6 @@ namespace Ejercicio_Prueba_GIT
     internal class Patatas
     {
         //Esta es la clase patatas para que quede claro que es una clase de prueba naa mas
+        //ahora estos son cambios para ver que pasa pues
     }
 }
