16d9f605198e6675683cc7bee71d3950097d861b
Miguel Cervera <l.riusaki15@gmail.com>
2026-03-30 10:42:39 +0200

    feat: anidar Patatas en la nueva rama

---
+++ Patatas.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_Prueba_GIT
+{
+    internal class Patatas
+    {
+        //Esta es la clase patatas para que quede claro que es una clase de prueba naa mas
+    }
+}
